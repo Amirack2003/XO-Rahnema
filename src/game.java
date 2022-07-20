25ff9ce5cbfa0e7691481cffdbac63b49d4abfac
amirack003 <amirack003@gmail.com>
2022-07-20 18:11:21 +0430

    Function yaftane barande bazi debug shod

--- src/game.java
+++ src/game.java
@@ -62,14 +62,19 @@ public class game {
         for(int x = 0 ; x < m ; x++) {
             for (int y = 0; y < m; y++) {
                 for(int z = 0 ; z < n ; z++) {
+                    if(!field[x][y].getBackground().equals(colors[z]))continue;
                     if ((x + 1 < m && y + 1 < m && field[x + 1][y + 1].getBackground().equals(colors[z])) && (x - 1 > m && y - 1 > m && field[x - 1][y - 1].getBackground().equals(colors[z]))) {
                         JOptionPane.showMessageDialog(null, "Nafare " + z + " Barande Shod");
+                        System.exit(0);
                     } else if ((x - 1 < m && y + 1 < m && field[x - 1][y + 1].getBackground().equals(colors[z])) && (x + 1 < m && y - 1 < m && field[x + 1][y - 1].getBackground().equals(colors[z]))) {
                         JOptionPane.showMessageDialog(null, "Nafare " + z + " Barande Shod");
+                        System.exit(0);
                     } else if ((x < m && y + 1 < m && field[x][y + 1].getBackground().equals(colors[z])) && (x < m && y - 1 < m && field[x][y - 1].getBackground().equals(colors[z]))) {
                         JOptionPane.showMessageDialog(null, "Nafare " + z + " Barande Shod");
+                        System.exit(0);
                     } else if ((x + 1 < m && y < m && field[x + 1][y].getBackground().equals(colors[z])) && (x - 1 < m && y < m && field[x - 1][y].getBackground().equals(colors[z]))) {
                         JOptionPane.showMessageDialog(null, "Nafare " + z + " Barande Shod");
+                        System.exit(0);
                     }
                 }
             }
